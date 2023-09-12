54a1753002b54d281042938cd8468cb8b78086cb
Marcelo <marcelo.arruda@in8.com.br>
2023-09-12 17:50:31 +0100

    split into 2 recursive functions

--- artworks/spiders/trial_spider.py
+++ artworks/spiders/trial_spider.py
@@ -28,26 +28,29 @@ class TrialSpider(scrapy.Spider):
         yield data
 
     def parse(self, response):
-        if response.url == 'http://pstrial-2019-12-16.toscrape.com/browse/':
-            for subcat in response.xpath('//*[(@id = "subcats")]/div'):
-                if subcat.css('h3::text').get() == "In Sunsh" or subcat.css('h3::text').get() == "Summertime":
-                    yield response.follow(subcat.css('a')[0], self.parse)
+        for subcat in response.xpath('//*[(@id = "subcats")]/div'):
+            if subcat.css('h3::text').get() == "In Sunsh" or subcat.css('h3::text').get() == "Summertime":
+                yield response.follow(subcat.css('a')[0], self.parse_subcat)
 
+    def parse_subcat(self, response):
+        subcat = response.xpath('//*[(@id = "subcats")]/div')
+        if subcat:
+            anchors = subcat.css('a')
+            yield from response.follow_all(anchors, callback=self.parse_subcat)
         else:
-            subcat = response.xpath('//*[(@id = "subcats")]/div')
-            if subcat:
-                anchors = subcat.css('a')
-                #nao ta pegando caso n exista subcategoria na classe
-                yield from response.follow_all(anchors, callback=self.parse)
-            else:
-                anchors = response.xpath('//*[@id="body"]/div[2]/a[contains(@href,"/item")]').css('a')
-                if anchors:
-                    yield from response.follow_all(anchors, callback=self.parse_art)
+            yield from self.parse_art_urls(response)
 
-                    next_page = response.css("[class='nav next']").css("[name='page']")[0].attrib['value']
-                    if response.url[-1:].isnumeric():
-                        split_url = response.url.split('page=')
-                        next_url = split_url[0] + 'page=' + next_page
-                        yield response.follow(next_url, callback=self.parse)
-                    else:
-                        yield response.follow(response.url + '?page=1', callback=self.parse)
+    def parse_art_urls(self, response):
+        # Extract art URLs
+        art_urls = response.xpath('//*[@id="body"]/div[2]/a[contains(@href,"/item")]').css('a')
+        if art_urls:
+            yield from response.follow_all(art_urls, callback=self.parse_art)
+
+        # Extract next page link
+        next_page = response.css("[class='nav next']").css("[name='page']")[0].attrib['value']
+        if response.url[-1:].isnumeric():
+            split_url = response.url.split('page=')
+            next_url = split_url[0] + 'page=' + next_page
+            yield response.follow(next_url, callback=self.parse_subcat)
+        else:
+            yield response.follow(response.url + '?page=1', callback=self.parse_subcat)
