6feb1407b5080d1fc542d5a4ec8f99dab5c1baa2
Marcelo <marcelo.arruda@in8.com.br>
2023-09-15 14:42:10 +0100

    delivery

--- artworks/enum/options.py
+++ artworks/enum/options.py
@@ -2,8 +2,6 @@ class Options:
     """
     A configuration class containing a list of predefined art categories.
 
-    This class provides a list of categories that can be used to filter the urls to scrape the data
-
     Attributes:
         categories (list of str): A list of art categories.
 
